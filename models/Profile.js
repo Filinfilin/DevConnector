5ce3bc5234c0b830a5f668733cbbec16a624d7cf
Filinfilin <gluboko.pod.vodoi@gmail.com>
2020-11-24 15:08:56 +0300

    update and create new profile request added

--- models/Profile.js
+++ models/Profile.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose')
 
+
 const ProfileSchema = new mongoose.Schema({
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'user'
     },
-    handle: {
-      type: String,
-      required: true,
-      max: 40
-    },
+
     company: {
       type: String
     },
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
-
+const validator = require('express-validator')
+const {check, validationResult} = require('express-validator')
 
 //@route GET api/profile/me
 //@desc get curent user profile
@@ -24,9 +25,75 @@ router.get('/me', auth, async (req, res) => {
     catch(err){
         console.error(err.message)
         res.status(500).send('server error from profile')
+    }});
+    //@route POST api/profile
+    //@desc create or update user profile
+    //@access private
+router.post('/', [auth,[check('status','Status is required').not().isEmpty(),
+                            check('skills', 'skills is required').not().isEmpty()
+                            ]],
+    async (req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        const { company,
+                website,
+                location,
+                bio,
+                status,
+                githubusername,
+                skills,
+                youtube,
+                twiter,
+                facebook,
+                instagram,
+                linkedin
+            } = req.body
 
+    //build profile object
+
+        const profileFields ={};
+        profileFields.user = req.user.id;
+        if(company) profileFields.company = company
+        if(website) profileFields.website = website
+        if(location) profileFields.location = location
+        if(bio) profileFields.bio = bio
+        if(status) profileFields.status = status
+        if(githubusername) profileFields.githubusername = githubusername
+        if(skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim())
+        }
+       profileFields.social = {}
+       if(youtube) profileFields.social.youtube = youtube
+       if(twiter) profileFields.social.twiter = twiter
+       if(facebook) profileFields.social.facebook = facebook
+       if(instagram) profileFields.social.instagram = instagram 
+       if (linkedin) profileFields.social.linkedin = linkedin
+
+       try{
+            let profile = await Profile.findOne({user: req.user.id})
+            if(profile){
+    //update profile
+                profile = await Profile.findOneAndUpdate(
+                     {user: req.user.id},
+                     {$set: profileFields},
+                     {new: true}
+                )
+                    return res.json(profile)
+            }
+
+    // create profile
+            profile = new Profile(profileFields)
+
+            await profile.save()
+            res.json(profile)
+
+       }catch(err){
+        console.error(err.message);
+        res.status(500).send('server error from profile update')
     }
-}
-);
+
+    })
 
 module.exports = router;
