53712466701c5b1440aba04ea1b2bed5e223bb62
Filinfilin <gluboko.pod.vodoi@gmail.com>
2020-11-30 23:32:01 +0300

    added post schema

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const { schema } = require('./Profile')
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+
+user: {
+   type: Schema.Types.ObjectId,
+   ref: "users"
+} ,
+text: {
+    type: String,
+    required: true
+},
+name:{
+    type: String
+},
+avatar:{
+    type: String
+},
+likes:[{
+        user: {
+            type: Schema.Types.ObjectId,
+            ref:"users"
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        name:{
+            type: String
+        },
+        avatar:{
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+
+})
+
+module.exports = Post = mongusee.model('post', PostSchema)
